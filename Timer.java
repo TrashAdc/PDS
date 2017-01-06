a4cde5c3f87896617e536d499346ac15779127c4
ryan v <benjamin.vanek@student.allenisd.org>
2017-01-05 23:17:32 -0600

    add 2 classes i made that might be useful
    timer class has a method that returns true when a specified amount of time has passed
    gettime class contains a bunch of methods to get the current time easier

---
+++ Timer.java
@@ -0,0 +1,61 @@
+import java.math.BigInteger;
+/**Created by ryan v on 1/4/2017.**/
+
+public class Timer {
+
+        public enum timeType { //enums for type of time
+            NANOSECONDS,
+            MILLISECONDS,
+            SECONDS,
+            MINUTES,
+            HOURS
+        }
+
+        private BigInteger startTime;
+        private BigInteger time;
+
+        public Timer(timeType type, int timeAmount){ //constructor
+            time = BigInteger.valueOf(timeAmount);
+            switch (type) {                             //this assigns how much time in nanoseconds will have to pass
+                case NANOSECONDS:                       //in order to make the timerFinish() method true
+                    time = BigInteger.valueOf(timeAmount);
+                    break;
+                case MILLISECONDS:
+                    time = time.multiply(BigInteger.valueOf(1000000));
+                    break;
+                case SECONDS:
+                    time = time.multiply(BigInteger.valueOf(1000000));
+                    time = time.multiply(BigInteger.valueOf(1000));
+                    break;
+                case MINUTES:
+                    time = time.multiply(BigInteger.valueOf(1000000));
+                    time = time.multiply(BigInteger.valueOf(1000));
+                    time = time.multiply(BigInteger.valueOf(60));
+                    break;
+                case HOURS:
+                    time = time.multiply(BigInteger.valueOf(1000000));
+                    time = time.multiply(BigInteger.valueOf(1000));
+                    time = time.multiply(BigInteger.valueOf(60));
+                    time = time.multiply(BigInteger.valueOf(60));
+                    break;
+                default:
+                    time = BigInteger.valueOf(0);
+                    break;
+            }
+        }
+
+        public void initTimer(){
+            startTime = BigInteger.valueOf(System.nanoTime()); //this starts the timer. only use this when you are ready
+        }                                                      //to begin.
+
+        public Boolean timerFinish(){
+            BigInteger cur = BigInteger.valueOf(System.nanoTime()); //this method checks to see whether the given time has passed.
+            cur = cur.subtract(startTime);                          //you must be checking this method constantly for the most
+            int finished = cur.compareTo(time);                     //accurate results.
+
+            if (finished == 0 || finished == 1)
+                return true;
+            else
+                return false;
+        }
+}
