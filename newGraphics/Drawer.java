feb632cb741f872e38b9f7415b04c27f5acffb20
ryan v <benjamin.vanek@student.allenisd.org>
2017-01-18 17:49:01 -0600

    updated to show off ImageLib class

--- newGraphics/Drawer.java
+++ newGraphics/Drawer.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * created by ryan v on 1/17/2017
@@ -8,8 +9,10 @@ import java.awt.*;
 //key input may need to be moved to a different class that handles game logic and positioning in the future
 public class Drawer extends JPanel {
 
-    private int x = 0; //a position of the text that is being drawn to thescreen
+    private int x = 0; //a position of the text that is being drawn to the screen
     private ActiveKeys key = new ActiveKeys(); //this is the key listener
+
+    private BufferedImage pichu = new ImageLib().getImage("pichu.png");
 
     public Drawer(){
         addKeyListener(key);
@@ -33,8 +36,10 @@ public class Drawer extends JPanel {
     public void paint(Graphics g){
         super.paint(g); //this resets everything on the screen. otherwise it will become one huge psychedelic mess. maybe that's what you want. i wont judge
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.blue);
+        g2d.setColor(Color.red);
+        g2d.drawImage(pichu, 100, 0, Window.SIZE.width, Window.SIZE.height, 0, -50, Window.SIZE.width, Window.SIZE.height, null);
         g2d.drawString("help me", x, 100); //this is all self explanatory
+
     }
 
     public void draw(){
