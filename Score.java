a15c28852ef7704fadd44efb12a2313883147ed4
Devin <devnyy@gmail.com>
2017-01-19 12:59:20 -0600

    UPDATED SCORE CLASS

--- Score.java
+++ Score.java
@@ -2,27 +2,52 @@
  * Created by Devin Popock on 1/17/2017.
  */
 public class Score {
+    private int P1Kill;
+    private int P2Kill;
+    private int stockCount1 = 3;
+    private int stockCount2 = 3;
+    private boolean KOP2;
+    private boolean KOP1;
+    private boolean stockmodeplaceholder;
+
     // Returns value of percent as a String
-    public String scoreConverter(int inputScore){
-        String percent= new String.valueOf(inputScore);
-        return percent;
+    public String scoreConverter(int inputScore) {
+        String converted = new String.valueOf(inputScore);
+        return converted;
 
     }
-    // Stores amount of kills by each player as a String
-    public int statKill(){
-        int P1Kill=0;
-        int P2Kill=0;
-        boolean KOP1=false;
-        boolean KOP2=false;
-        if(KOP2==true){
-            P1Kill++;
-            KOP2=false;
-        }
-        if(KOP1==true){
-            P2Kill++;
-            KOP1=false;
-        }
 
+    // Stores amount of kills by Player1/Player2 as an int
+    public int statKillP1() {
+        KOP2 = false;
+        if (KOP2 == true) {
+            P1Kill++;
+            KOP2 = false;
+        }
         return P1Kill;
+    }
+
+    public int statKillP2() {
+        KOP1 = false;
+        if (KOP1 == true) {
+            P2Kill++;
+            KOP2 = false;
+        }
+        return P2Kill;
+    }
+
+    // THIS METHOD IS FOR STOCK MODE ONLY
+    public int stockHolder1() {
+        if (stockmodeplaceholder == true) {
+
+                if (KOP2 == true) {
+                    stockCount1--;
+                    KOP2 = false;
+                }
+            }
+        return stockCount1;
+
+
+
     }
 }
