04eee5a9c29ca729dbaa321aff5f2a02d559df25
ryan v <benjamin.vanek@student.allenisd.org>
2017-01-27 11:03:52 -0600

    Delete MainClass.java

--- MainClass.java
+++
@@ -1,9 +0,0 @@
-/**
- * created by ryan v on 1/17/2017
- **/
-public class MainClass {
-    public static void main(String[] args){
-        Window gameWindow = new Window();
-        new Thread(gameWindow).start();
-    }
-}
