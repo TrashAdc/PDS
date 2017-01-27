16849924e0723e94d37c2f2d45ce430614a19bc7
TrashAdc <dreamwin777@gmail.com>
2017-01-27 11:25:28 -0600

    Math and Physics Class
    Improved gravity class accommodating falling speeds.(Still Hypothetical)

--- game/core/src/com/mygdx/game/math.java
+++ game/core/src/com/mygdx/game/math.java
@@ -1,4 +1,3 @@
-package com.mygdx.game;
 /**
  * Created by 250065 on 1/17/2017.
  */
@@ -10,7 +9,8 @@ public class math {
 
     public int Gravity(){
         gravity=100;
-        velocity=1080;
+        int kineticenrgy=gety
+        velocity=Math.sqrt((2*kineticenrgy)/weight);
         initial_speed=0;
         for(int i=30;i<=30;i--) {
             //velocity=modifier
@@ -19,5 +19,20 @@ public class math {
         }
      return velocity;
 
+    }
+    public void Knockback() {
+        boolean fly = true;
+        if (sprite is not dead){
+            while (percent >= 0) {
+
+                double launchangle =/**pixel of collision is the vector magnitude*/arc tan(y / x);
+
+            }
+
+        }
+    }
+    public int percent(){
+
+        dmg= spritestat *scale+(percent);
     }
 }
